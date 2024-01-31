c3070783d2f1667f0da6bd050ffff136ba7932cb
Amalekh Biju <bamalekh@gmail.com>
2024-01-31 22:21:36 +0530

    Nav Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
-import About from './components/about/About';
+import Article from './components/article/Article';
 import Nav from './components/nav/Nav';
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/article" element={<Article />} />
         </Routes>  
       </Router>
     </div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,60 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './nav.css'
-import { Helmet } from 'react-helmet'
-import { FaHome } from "react-icons/fa";
-import LOGO from '../../assets/logosiaa.png'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { HiOutlineMenuAlt1 } from 'react-icons/hi'
-import { PiEyeClosedBold } from 'react-icons/pi'
-import './nav.js'
-
-const clear = (el) => {
-	const dad = el.parentElement;
-	for (let i of dad.children) {
-		i.className = '';
-	}
-};
-const navElements = ['home', 'article', 'search', 'user'];
-
-
 
 const Nav = () => {
-useEffect(() => {
-		document.querySelectorAll('.container ul li').forEach(
-		  (e, i) =>
-			(e.onclick = function () {
-			  clear(this);
-			  this.classList.toggle('active');
-			  e.classList.add(navElements[i]);
-			})
-		);
-	  }, []);
-	
+  const [activeNav, setActiveNav]= useState('/')
   return (
-    <div className="nav">
-		<div class="container">
-			<ul>
-				<li class="active home">
-					<i class="fa fa-home"></i>
-					<p>Home</p>
-				</li>
-				<li>
-				<i class="fa fa-newspaper"></i>
-					<p>Articles</p>
-				</li>
-				<li>
-					<i class="fa fa-search"></i>
-					<p>
-						Search
-					</p>
-				</li>
-				<li>
-					<i class="fa fa-user"></i>
-					<p>User</p>
-				</li>
-			</ul>
-		</div>
-    </div>
+    <nav>
+      <Link to="/" onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}><AiOutlineHome className='icon'/> <p>Home</p></Link>
+      <Link to="/article" onClick={() => setActiveNav('/article')} className={activeNav === '/article' ? 'active' : ''}><AiOutlineUser className='icon'/><p>Articles</p></Link>
+      <Link to="/" onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}><AiOutlineHome className='icon'/> <p>Home</p></Link>
+      <Link to="/" onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}><AiOutlineHome className='icon'/> <p>Home</p></Link>
+    </nav>
   )
 }
 
