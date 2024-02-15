144a401772fea92e7ae749aff204b02a142035c4
Amalekh-Biju <bamalekh@gmail.com>
2024-02-15 15:13:19 +0530

    Title Break Added

--- src/components/cal/Cal.jsx
+++ src/components/cal/Cal.jsx
@@ -8,7 +8,7 @@ const Cal = () => {
 		<div id='stars2'></div>
 		<div id='stars3'></div>
 
-<iframe id='calen' src="https://calendar.zoho.in/zc/ui/embed/#calendar=zz08021230f3facd698535d10ee0a76cff1156243b8ae7dfab49d38a1d9541707bc2079135223c4514c8a94afd57d42cb067bdf2dc&title=Space%20Club&type=6&language=en&timezone=Asia%2FKolkata&showTitle=1&showTimezone=1&startingDayOfWeek=0&timeFormat=0&color=6"width="1350" height="720" frameBorder="0" scrolling="no"></iframe>
+<iframe id='calen' src="https://calendar.zoho.in/zc/ui/embed/#calendar=zz08021230f3facd698535d10ee0a76cff1156243b8ae7dfab49d38a1d9541707bc2079135223c4514c8a94afd57d42cb067bdf2dc&title=Space%20Club&type=6&language=en&timezone=Asia%2FKolkata&showTitle=1&showTimezone=1&startingDayOfWeek=0&timeFormat=0&color=6"width="1350" height="720" frameBorder="0" scrolling="no" loading='eager'></iframe>
     </div>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,60 +1,62 @@
-import React from 'react'
-import { ReactDOM } from 'react';
-import './home.css';
-import { Link } from 'react-router-dom';
-import { useState } from 'react'
-import '../nav/nav.css'
-import { motion } from "framer-motion";
-const Home = () => {
-	// const text1= "S.p.a.c.e. .C.l.u.b".split(".");
-	const text2 = "S.I.A.  .M.I.T.S".split(".");
-  const [activeNav, setActiveNav]= useState('/solar')
-  return (
-	<div class="Home">
-		 <div id='stars'></div>
-		<div id='stars2'></div>
-		<div id='stars3'></div>
+	import React from 'react'
+	import { ReactDOM } from 'react';
+	import './home.css';
+	import { Link } from 'react-router-dom';
+	import { useState } from 'react'
+	import '../nav/nav.css'
+	import { motion } from "framer-motion";
+	const Home = () => {
+		const text2 = "S.I.A.".split(".");
+		const text1= "M.I.T.S".split(".");
 		
-		<div id='title'>
-			<span>
-			Welcome To 
-			</span>
-			<br/><div>
-			{
-				text2.map((el2, i) => (
-					<motion.span className='h1'
-					
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{
-						duration: 0.25,
-						delay: i / 10,
-					}}
-					key={i}
-					>
-			          {el2}{" "}
-        </motion.span>
-      ))}
-	  </div>
+	const [activeNav, setActiveNav]= useState('/solar')
+	return (
+		<div class="Home">
+			<div id='stars'></div>
+			<div id='stars2'></div>
+			<div id='stars3'></div>
+			
+			<div id='title'>
+				<span>
+				Welcome To 
+				</span>
+				<br/><div>
+				{
+					text2.map((el2, i) => (
+						<motion.span className='h1'
+						
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{
+							duration: 4,
+							delay: i /10,
+						}}
+						key={i}
+						>
+						{el2}{""}
+			</motion.span>
+		))}
+		<br/>
+		{
+			text1.map((el, i) => (
+				<motion.span className='h1'
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1	 }}
+				transition={{
+					duration: 4,
+					delay: i /3,
+				}}
+				key={i}
+				>
+			{el}{""}
+		</motion.span>
+		))}
 		
-				<br/>
-		{/* {
-		text1.map((el, i) => (
-        <motion.span className='h1'
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 0 }}
-          transition={{
-            duration: 1.25,
-            delay: i /10,
-          }}
-          key={i}
-        >
-          {el}{" "}
-        </motion.span>
-      ))} */}
 		</div>
-	</div>
-  )
-}
+			
+			</div>
+		</div>
+	)
+	}
 
-export default Home
+	export default Home
