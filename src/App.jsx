af586045d42ee554b5c9f1a424d13d6f458d168f
Bhavan S <109290304+parzi-al@users.noreply.github.com>
2024-02-15 12:00:31 +0530

    home animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './components/home/Home';
 import Article from './components/article/Article';
 import Nav from './components/nav/Nav.jsx';
 import Solar from './components/solar-system/Solar.jsx';
-import Cal from './components/cal/Cal.jsx';
 
 const App = () => {
   return (
@@ -15,7 +14,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/article" element={<Article />} />
           <Route path="/solar" element={<Solar />} />
-          <Route path="/cal" element={<Cal />} />
         </Routes>  
       </Router>
     </div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
-import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
-import { SlCalender } from "react-icons/sl";
+import {BiMessageSquareDetail} from 'react-icons/bi'
 import {GiGalaxy} from 'react-icons/gi'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -14,7 +13,6 @@ const Nav = () => {
       <Link to="/" onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}><AiOutlineHome className='icon'/> <p>Home</p></Link>
       <Link to="/article" onClick={() => setActiveNav('/article')} className={activeNav === '/article' ? 'active' : ''}><BiMessageSquareDetail className='icon'/><p>Articles</p></Link>
       <Link to="/solar" onClick={() => setActiveNav('/solar')} className={activeNav === '/solar' ? 'active' : ''}> <GiGalaxy className="icon" /><p>Our Solar System</p></Link>
-      <Link to="/cal" onClick={() => setActiveNav('/cal')} className={activeNav === '/cal' ? 'active' : ''}><SlCalender className='icon'/> <p>Calender</p></Link>
 
     </nav>
   )
