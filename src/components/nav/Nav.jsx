e7109e8fdebc1067e9eb3290a0343f55a20c162e
Amalekh-Biju <bamalekh@gmail.com>
2024-10-08 22:21:34 +0530

    Corrected css

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,10 +15,7 @@ const Nav = () => {
       <Link to="/" onClick={() => setActiveNav('/')} className={`flex items-center rounded-full p-3 transition-all duration-500 ${activeNav === '/' ? 'bg-black text-white ' : 'bg-transparent text-black'}`}><AiOutlineHome className='icon'/> <p className={`${activeNav === '/' ? '' : 'hidden'}`}>Home</p></Link>
       <Link to="/article" onClick={() => setActiveNav('/article')} className={`flex items-center rounded-full p-3 transition-all duration-500 ${activeNav === '/article' ? 'bg-black text-white' : 'bg-transparent text-black'}`}><BiMessageSquareDetail className='icon'/><p className={`${activeNav === '/article' ? '' : 'hidden'}`} >Articles</p></Link>
       <a href="/solar.html"  className={`flex items-center rounded-full p-3 transition-all duration-500 ${activeNav === '/solar' ? 'bg-black text-white' : 'bg-transparent text-black'}`}> <GiGalaxy className="icon" /></a>
-
       <Link to="/cal" onClick={() => setActiveNav('/cal')}className={`flex items-center rounded-full p-3 transition-all duration-500 ${activeNav === '/cal' ? 'bg-black text-white' : 'bg-transparent text-black'}`}><SlCalender className='icon'/> <p className={`${activeNav === '/cal' ? '' : 'hidden'}`}>Calender</p></Link>
-
-
     </nav>
   )
 }
